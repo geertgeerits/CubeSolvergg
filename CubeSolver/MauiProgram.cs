a37bcf309c1673a2d8018f55119b2487a9ca8197
Geert Geerits <ggeerits@hotmail.com>
2026-08-21 11:08:44 +0200

    Update Microsoft.Maui.Controls to version=10.0.100 and rubik to cubesolver

--- CubeSolver/MauiProgram.cs
+++ CubeSolver/MauiProgram.cs
@@ -14,7 +14,6 @@ namespace CubeSolver
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                    fonts.AddFont("PublicSans-Bold.ttf", "PublicSansBold");
                 })
 
                 .ConfigureLifecycleEvents(static events =>
